c8bc75691cd624426fef4ce57cbf38e446ab711d
Calhuang <calhuang@ucdavis.edu>
2019-03-20 11:04:53 -0700

    docker

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express")
 const http = require("http")
-const socketIo = require("socket.io")
-const axios = require("axios")
 const port = process.env.PORT || 4001
 const index = require("./routes/index")
 const app = express()
@@ -9,15 +7,6 @@ const app = express()
 app.use(index)
 
 const server = http.createServer(app)
-
-const io = socketIo(server)
-
-io.on('connection', socket => {
-  console.log('Client connected')
-  socket.on('disconnect', () => {
-    console.log('Client disconnected')
-  })
-})
 
 app.listen(port, (err) => {
   if (err) {
